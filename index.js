be09171cb51fd69a5fa316be119a64aa9d5890de
tanzil.ncpsc@gmail.com <tanzil.darkcode@gmail.com>
2022-12-21 17:14:36 +0600

    refactor: move main code to ./src/bot.js

--- index.js
+++ index.js
@@ -1,47 +1,5 @@
 require("dotenv").config();
-const {
-  Client,
-  GatewayIntentBits,
-  Partials,
-  Collection,
-} = require("discord.js");
+const client = require("./src/bot");
 
-// Initialize the client
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
-const { User, Message, GuildMember, ThreadMember } = Partials;
-const client = new Client({
-  intents: [Guilds, GuildMembers, GuildMessages],
-  partials: [Message, ThreadMember],
-});
-
-client.events = new Collection();
-client.commands = new Collection();
-client.subCommands = new Collection();
-client.components = new Collection();
-client.guildConfig = new Collection();
-client.color = "#1975FC";
-
-// Establish connection to Database
-const { connect } = require("mongoose");
-connect(process.env.DATABASE_URI, {
-  connectTimeoutMS: 10000,
-})
-  .then((connection) => {
-    console.log(`Connected to database: ${connection.connection.name}`);
-  })
-  .catch((err) => {
-    console.log(err);
-    console.log("⚠️ Failed to establish connection to database. exiting...");
-    process.exit(1);
-  });
-
-// loading the handlers
-const { loadEvents } = require("./src/handlers");
-loadEvents(client);
-
-// load guild config
-const { loadGuildMemberLogConfig } = require("./src/functions/loadConfig");
-
-loadGuildMemberLogConfig(client);
 // Login to the client
 client.login(process.env.DISCORD_TOKEN);
---
+++ src/bot.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
+
+// Initialize the client
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [Message, ThreadMember],
+});
+
+client.events = new Collection();
+client.commands = new Collection();
+client.subCommands = new Collection();
+client.components = new Collection();
+client.guildConfig = new Collection();
+client.color = "#1975FC";
+
+// Establish connection to Database
+const { connect } = require("mongoose");
+connect(process.env.DATABASE_URI, {
+  connectTimeoutMS: 10000,
+})
+  .then((connection) => {
+    console.log(`Connected to database: ${connection.connection.name}`);
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("⚠️ Failed to establish connection to database. exiting...");
+    process.exit(1);
+  });
+
+// loading the handlers
+const { loadEvents } = require("./handlers");
+loadEvents(client);
+
+// load guild config
+const { loadGuildMemberLogConfig } = require("./functions/loadConfig");
+
+loadGuildMemberLogConfig(client);
+
+module.exports = client;
